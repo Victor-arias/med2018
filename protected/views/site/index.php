bf5befebda1ab4b3871d1a2e5c050fd88ee828a0
Selene Estrada <seleneestrada@gmail.com>
2013-06-20 11:03:31 -0500

    Cambio en el home para anunciar ganadores.
    Se cambiaron los textos en el home y las imágenes en la barra lateral.
    Se cambió el video por el que muestra cómo los ganadores se enteraron.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,12 +5,13 @@ $this->pageTitle = Yii::app()->name;
 ?>
 <div id="content">
 	<div id="intro-home">
-		<p>¡Tú podrás ser uno de los niños que viaje hasta Suiza para conocer si la ciudad será la sede de los Juegos Olímpicos de la juventud Medellín 2018!  Gracias a la Alcaldía de Medellín,  la candidatura de los Juegos, la Secretaría de educación, Telemedellín y este entretenido juego.</p>
+		<p>Ya sabemos quiénes se van hasta Suiza para conocer si la ciudad será la sede de los Juegos Olímpicos de la juventud Medellín 2018. La Alcaldía de Medellín, la candidatura de los Juegos, la Secretaría de educación y Telemedellín felicitan a los ganadores y agradecen a todos los participantes que se contagiaron del espíritu olímpico.
+</p>
 	</div>
 	<div id="left-content">
-		<p class="titulo-video">En este video puedes ver cómo ser uno de los ganadores</p>
+		<p class="titulo-video">Los ganadores se van a Suiza y así recibieron la noticia</p>
 
-		<iframe width="560" height="315" src="http://www.youtube.com/embed/ZE7CZzmlb1s?rel=0" frameborder="0" allowfullscreen></iframe>
+		<iframe width="560" height="420" src="http://www.youtube.com/embed/AWf5fv51Vo8" frameborder="0" allowfullscreen></iframe>
 	</div>
 	<div id="right-content">
 		<ul>
@@ -18,8 +19,17 @@ $this->pageTitle = Yii::app()->name;
 				<?php //echo CHtml::link('<span class="resaltado">Regístrate</span> y empieza a jugar', array('registro'), array('class' => 'registrate') )?>
 			</li>-->
 			<li>
-				<?php echo CHtml::link('¡Estamos verificando <span class="resaltado">los resultados!</span>', array('/puntajes'), array('class' => 'juega'))?>
+				<?php /*?><?php echo CHtml::link('¡Estamos verificando <span class="resaltado">los resultados!</span>', array('/puntajes'), array('class' => 'juega'))?><?php */?>
 			</li>
+            <li>
+             	<div class="nina">
+                </div>
+            </li>
+            
+            <li>
+             	<div class="nino">
+                </div>
+            </li>
 		</ul>
 	</div>
 </div>
